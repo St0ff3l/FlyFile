86d7224886ff0e0179224b605b51df89488a1f05
St0ff3l <chenzekaiczkczk@gmail.com>
2026-01-17 19:28:27 +0800

    Add initial Flutter project structure and configuration files

---
+++ backend/src/test/java/com/flyfile/backend/FlyFileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyfile.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyFileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
